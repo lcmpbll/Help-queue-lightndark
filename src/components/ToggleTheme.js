2c091a2e841a1a552b688dcc127c9e2abe099fad
Liam Cammpbell <liam22campbell@gmail.com>
2022-09-26 13:43:57 -0700

    Add dark and light themes
    Co-authored-by: joeroaden <joeroaden@users.noreply.github.com>
    Co-authored-by: Garrett Hays <GarrettHays@users.noreply.github.com>

---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function ToggleTheme(props) {
+  const { toggleTheme } = props;
+  return (
+    <React.Fragment>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+ToggleTheme.propTypes = {
+  toggleTheme: PropTypes.func
+}
+
+export default ToggleTheme;
